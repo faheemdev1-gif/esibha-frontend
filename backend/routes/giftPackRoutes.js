486a8f84e82ab12bcb1b6dd043ea6a44917f4e76
faheemdev1-gif <faheemdev1@gmail.com>
2026-06-18 15:25:13 +0500

    Stop tracking backend in frontend repo

--- backend/routes/giftPackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const { getGiftPacks } = require("../controllers/giftPackController");
-
-router.get("/", getGiftPacks);
-
-module.exports = router;
