b258285f750814908b680485e0233c52e144f0a9
faheemdev1-gif <faheemdev1@gmail.com>
2026-06-18 15:57:42 +0500

    Fix production API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "wheat-eagle-393999.hostingersite.com",
+  baseURL: "https://wheat-eagle-393999.hostingersite.com",
 });
 
 export default api;
